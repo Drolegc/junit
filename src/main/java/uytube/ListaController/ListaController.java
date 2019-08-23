51a7b701b704dc29756add8fe9991f286bf1e26f
drolegc <drolegc@gmail.com>
2019-08-23 11:08:25 -0300

    Manager en models

--- src/main/java/uytube/ListaController/ListaController.java
+++ src/main/java/uytube/ListaController/ListaController.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
-import Manager.Manager;
+import uytube.models.manager.Manager;
 import uytube.models.Canal;
 import uytube.models.Lista;
 
@@ -20,18 +20,7 @@ public class ListaController implements ILista {
 		// TODO Auto-generated method stub
 		
 		//POR EL MOMENTO ESTAMOS CREANDO UNA LISTA DEFAULT
-		EntityManager em = mng.getEntityManager();
 		
-		try {
-			Lista lista = (Lista) em.createQuery("From Lista where nombre = :name").setParameter("name", listaName).getSingleResult();
-		}catch(Exception NoResultException) {
-			List<Canal> canales = (List<Canal>)em.createQuery("From Canal").getResultList();
-			Lista l = new Lista(listaName,true,null);
-			for(Canal c: canales) {
-				c.addLista(l);
-			}
-			System.out.println("Nueva lista creada para todos los usuarios");
-		}
 		
 		
 	}
