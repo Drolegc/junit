9c03c1441955349d073ba1c398c5e45f0533d45f
Emiliano Bayona <emii.bayona22@gmai.com>
2019-08-29 19:55:36 -0300

    Modificar Lista interface

--- src/main/java/uytube/views/listas/modificar.java
+++ src/main/java/uytube/views/listas/modificar.java
@@ -3,8 +3,11 @@ package uytube.views.listas;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
+import uytube.ListaController.ILista;
+import uytube.ListaController.ListaController;
 import uytube.UsuarioController.IUsuario;
 import uytube.UsuarioController.UsuarioController;
+import uytube.models.Lista;
 import uytube.models.Usuario;
 import uytube.views.Frame;
 
@@ -17,6 +20,8 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 public class modificar extends JPanel {
 
@@ -34,19 +39,23 @@ public class modificar extends JPanel {
 		}
 		
 		JComboBox comboBox = new JComboBox(array);
+
 		comboBox.setBounds(34, 55, 150, 24);
 		add(comboBox);
 		
 		JButton btnModificar = new JButton("Modificar");
-		btnModificar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
 		btnModificar.setBounds(293, 55, 114, 25);
 		add(btnModificar);
 		
 		JComboBox comboBox_1 = new JComboBox();
+		
+		ILista controllerLista = new ListaController();
+		
+		List<Lista> listas = controllerLista.listarListas(usuarios.get(comboBox.getSelectedIndex()).getNickname());
+		
+		comboBox_1.setModel(new DefaultComboBoxModel(this.ListasToArr(listas)));
 		comboBox_1.setBounds(34, 172, 150, 24);
+		
 		add(comboBox_1);
 		
 		JLabel lblUsuario = new JLabel("Usuario");
@@ -68,6 +77,35 @@ public class modificar extends JPanel {
 		});
 		btnVolver.setBounds(293, 249, 114, 25);
 		add(btnVolver);
+		
+		comboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				
+				if(e.getStateChange() == ItemEvent.SELECTED) {
+					//Se cambio el usuario
+					
+					//obtener nickname
+					
+					String nombreUser = usuarios.get(comboBox.getSelectedIndex()).getNombre();
+					System.out.println(nombreUser);
+					
+					//buscar las lsitas del usuario
+					
+					comboBox_1.setModel(new DefaultComboBoxModel(ListasToArr(listas)));
+					comboBox_1.revalidate();
+					comboBox_1.repaint();
+				}
+				
+			}
+		});
 
+	}
+	
+	private String[] ListasToArr(List<Lista> listas ) {
+		String[] arr = new String[listas.size()];
+		for(int i = 0;i<listas.size();i++) {
+			arr[i] = listas.get(i).getNombre();
+		}
+		return arr;
 	}
 }
