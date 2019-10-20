35471d03c923bee717d102dea341fcfd969d6b8c
Daniel <wm1293@gmail.com>
2019-10-20 01:32:19 -0300

    TestCategoriaControllerCompleto

--- src/main/java/uytube/CategoriaController/ICategoria.java
+++ src/main/java/uytube/CategoriaController/ICategoria.java
@@ -9,8 +9,8 @@ import uytube.models.Video;
 public interface ICategoria {
 	
 	public boolean altaCategoria(String nombre);
-	public List<Video> consultarCategoria(String nombreCategoria);
-	public void listarCategoriasExistentes();
+	//public List<Video> consultarCategoria(String nombreCategoria);
+	//public void listarCategoriasExistentes();
 	public ArrayList<Categoria> listarCategorias();
 	public String[] listarCategoriasName();
 	public void modificarCategoria(Categoria nuevoNombre);
